6e52a89d33466ab9ec3fb0cb3b30cee7839757d0
Wilder Leon <wleona@ripley.com>
2022-11-11 11:04:58 -0300

    feat: Update active event

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { onAddNewEvent, onSetActiveEvent } from '../store'
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from '../store'
 
 export const useCalendarStore = () => {
   const { events, activeEvent } = useSelector((state) => state.calendar)
@@ -15,6 +15,7 @@ export const useCalendarStore = () => {
     // If everything goes well
     if (calendarEvent._id) {
       // Updating
+      dispatch(onUpdateEvent(calendarEvent))
     } else {
       // Creating
       dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }))
