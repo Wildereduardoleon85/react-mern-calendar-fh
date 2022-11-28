315189e258aa3e3fb0343e2e48d5d2e799c2d0fe
Wilder Leon <wleona@ripley.com>
2022-11-28 19:47:38 -0300

    feat: Login & Register forms

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,14 +3,14 @@ import { LoginPage } from '../auth'
 import { CalendarPage } from '../calendar'
 
 export const AppRouter = () => {
-  const isAuthenticated = true
+  const authStatus = 'not-authenticated'
 
   return (
     <Routes>
-      {isAuthenticated ? (
-        <Route path='/*' element={<CalendarPage />} />
+      {authStatus === 'not-authenticated' ? (
+        <Route path='/auth/*' element={<LoginPage />}></Route>
       ) : (
-        <Route path='auth/*' element={<LoginPage />} />
+        <Route path='/*' element={<CalendarPage />}></Route>
       )}
 
       <Route path='/*' element={<Navigate to='/auth/login' />} />
